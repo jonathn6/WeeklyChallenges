406b3acce7dcaeb31f16759f7dabb4f3f759f148
jonathn6 <jonathn6@icloud.com>
2021-11-08 16:34:31 -0500

    Weekly Challenge set 3

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -7,47 +7,124 @@ namespace ChallengesWithTestsMark8
     {
         public bool ArrayContainsAFalse(bool[] vals)
         {
-            throw new NotImplementedException();
+            for (var i = 0; i < vals.Length; i++)
+            {
+                if (vals[i] == false)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
-            throw new NotImplementedException();
+            
+            int sum = 0;
+            try
+            {
+                foreach (var num in numbers)
+                {
+                    if (num%2!=0)
+                    {
+                        sum += num;
+                    }
+                }
+                if (sum%2!=0)
+                {
+                    return true;
+                } else
+                {
+                    return false;
+                }
+            }
+            catch
+            { return false; }
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
-            throw new NotImplementedException();
+            char[] ch = password.ToCharArray();
+            int result = 0;
+            char tempLetter = 'n';
+            bool hasUpper = false;
+            bool hasLower = false;
+            bool hasNumber = false;
+
+            for (var i=0;i<ch.Length;i++)
+            {
+                if (int.TryParse(Char.ToString(ch[i]), out result))
+                {
+                    hasNumber = true;
+                } else
+                {
+                tempLetter = Char.ToUpper(ch[i]);
+                    if (tempLetter==ch[i])
+                    {
+                        hasUpper = true;
+                    }
+                    tempLetter = Char.ToLower(ch[i]);
+                    if (tempLetter == ch[i])
+                    {
+                        hasLower = true;
+                    }
+                }
+            }
+
+            if (hasUpper == hasLower && hasLower == hasNumber && hasNumber == true)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
         }
 
         public char GetFirstLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            char[] tempString = val.ToCharArray();
+            return tempString[0];
         }
 
         public char GetLastLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            char[] tempString = val.ToCharArray();
+            return tempString[val.Length-1];
         }
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
-            throw new NotImplementedException();
+            decimal result  = divisor==0 ?  0 :  dividend / divisor;
+            return result;
         }
 
         public int LastMinusFirst(int[] nums)
         {
-            throw new NotImplementedException();
+            return nums[nums.Length - 1] - nums[0];
+
         }
 
         public int[] GetOddsBelow100()
         {
-            throw new NotImplementedException();
+            int[] oddsBelow100 = new int[50];
+            int index = -1;
+
+            for (var i=1;i<100;i=i+2)
+            {
+                oddsBelow100[++index] = i;
+            }
+
+            return oddsBelow100;
+
         }
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
-            throw new NotImplementedException();
+            for (var i=0;i<words.Length;i++)
+                {
+                    words[i] = words[i].ToUpper();
+                }
+            return;
         }
     }
 }
